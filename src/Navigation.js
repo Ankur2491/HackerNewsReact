931e278e0367cd45d537650884e551adc55286e8
Ankur Sharma <ankur.101045@gmail.com>
2023-10-19 16:12:00 +0530

    reduced nav padding

--- src/Navigation.js
+++ src/Navigation.js
@@ -12,12 +12,12 @@ export default function Navigation() {
             <Toolbar disableGutters>
           <Link style={{ color: '#F5F5F5', fontSize:'20px', textDecoration:'none' }} onClick={()=>setCat('new')}>HackerNews</Link>
           <Divider orientation="vertical" variant="middle" flexItem />
-          <Link style={{ color: cat === 'new'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'5px' }} onClick={()=>setCat('new')}>New</Link>
-          <Link style={{ color: cat === 'top'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'5px' }} onClick={()=>setCat('top')}>Top</Link>
-          <Link style={{ color: cat === 'best'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'5px' }} onClick={()=>setCat('best')}>Best</Link>
-          <Link style={{ color: cat === 'show'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'5px' }} onClick={()=>setCat('show')}>Show</Link>
-          <Link style={{ color: cat === 'ask'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'5px' }} onClick={()=>setCat('ask')}>Ask</Link>
-          <Link style={{ color: cat === 'search'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'5px' }} onClick={()=>setCat('search')}>Search</Link>
+          <Link style={{ color: cat === 'new'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'2px' }} onClick={()=>setCat('new')}>New</Link>
+          <Link style={{ color: cat === 'top'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'2px' }} onClick={()=>setCat('top')}>Top</Link>
+          <Link style={{ color: cat === 'best'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'2px' }} onClick={()=>setCat('best')}>Best</Link>
+          <Link style={{ color: cat === 'show'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'2px' }} onClick={()=>setCat('show')}>Show</Link>
+          <Link style={{ color: cat === 'ask'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'2px' }} onClick={()=>setCat('ask')}>Ask</Link>
+          <Link style={{ color: cat === 'search'? 'grey': 'white', fontSize:'15px', textDecoration:'none', paddingTop:'5px', paddingLeft:'2px' }} onClick={()=>setCat('search')}>Search</Link>
             </Toolbar>
         </AppBar>
         {
